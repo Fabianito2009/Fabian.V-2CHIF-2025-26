72023e073471dbf1b693f073737eaf1c7d28c28a
Fabian Vallant <f.vallant@students.htl-leonding.ac.at>
2025-09-25 13:51:21 +0200

    calculate

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,26 +1,8 @@
-const counterbutton = document.getElementById("counterButton") as HTMLButtonElement;
-const counter = document.getElementById(`counter`) as HTMLParagraphElement;
-const counterdegrease = document.getElementById("count") as HTMLButtonElement;
-const Imgrement = document.getElementById('Imgrement') as HTMLInputElement;
 const select = document.getElementById('ImgrementSelect') as HTMLSelectElement;
+const calculate = document.getElementById("calculate") as HTMLButtonElement;
  
-let clickcounter = 0;
-select.addEventListener('change', () => {
-    Imgrement.value = select.value
-})
  
-counterbutton.addEventListener('click', () => {
-    clickcounter += parseInt(Imgrement.value);
-    counter.textContent = `${clickcounter}`
-    if (clickcounter >= 0){
-        counter.style.color = 'black';
-    }
-})
-counterdegrease.addEventListener('click', () => {
-    clickcounter -= parseInt(Imgrement.value);
-    counter.textContent = `${clickcounter}`
-    if (clickcounter < 0){
-        counter.style.color = 'red';
-    }
+calculate.addEventListener('click', () => {
+ 
 })
  
