b457d25760191a402fb32f8b100cf0bccdfe2665
Fabian Vallant <f.vallant@students.htl-leonding.ac.at>
2025-09-25 14:15:33 +0200

    calculater

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,8 +1,26 @@
-const select = document.getElementById('ImgrementSelect') as HTMLSelectElement;
-const calculate = document.getElementById("calculate") as HTMLButtonElement;
- 
- 
+const result = document.getElementById('result') as HTMLParagraphElement;
+const calculate = document.getElementById('calculate') as HTMLButtonElement;
+const firstNumber = document.getElementById('firstNum') as HTMLInputElement;
+const secondNumber = document.getElementById('secondNum') as HTMLInputElement;
+const operations = document.getElementById('operation') as HTMLInputElement;
+
+
 calculate.addEventListener('click', () => {
- 
-})
- 
+  if (firstNumber.value !== '' && secondNumber.value !== '') {
+    if (operations.value === '+') {
+      result.textContent = String(parseInt(firstNumber.value) + parseInt(secondNumber.value));
+    } else if (operations.value === '-') {
+      result.textContent = String(parseInt(firstNumber.value) - parseInt(secondNumber.value));
+    } else if (operations.value === '*') {
+      result.textContent = String(parseInt(firstNumber.value) * parseInt(secondNumber.value));
+    } else if (operations.value === '/') {
+      if (secondNumber.value === '0') {
+        result.textContent = "You can't divide by Zero!";
+        return;
+      }
+      result.textContent = `${String(parseInt(firstNumber.value) / parseInt(secondNumber.value))}`;
+    }
+  }else{
+    result.textContent = "Please fill in the numbenrs!";
+  }
+});
